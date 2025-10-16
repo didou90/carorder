7ebe591f8ebe07543e3a5c7f651589fd8cabd082
unknown <saad.abdelmadjid@gmail.com>
2025-10-16 13:48:47 +0100

    setting.py updated

--- rms/settings.py
+++ rms/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("SECRET_KEY")
 DEBUG = True
 
 
-ALLOWED_HOSTS = ['djangosupermarket.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['djangosupermarket.herokuapp.com','127.0.0.1', 'carorder-1.onrender.com']
 
 
 # Application definition
